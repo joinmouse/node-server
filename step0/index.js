c765d5a07baf27ca7000cdb809dbe4fbfb93c35a
wuqi <908380434@qq.com>
2017-09-18 21:49:30 +0800

    用node的http模块搭建一个服务器

---
+++ step0/index.js
@@ -0,0 +1,23 @@
+//引入http模块
+const http = require('http')
+//参考链接：http://nodejs.cn/api/http.html
+
+let server = http.createServer(function(request,response){
+  //设置响应头部内容
+  response.setHeader('Content-Type','text/html')
+  //response.setHeader('Content-Type','text/plain' charset='utf-8')
+
+  response.write('<html><head><meta charset="utf-8" /></head>')
+  response.write('<body>')
+  response.write('<h1>你好</h1>')
+  response.write('</body>')
+
+  response.end()
+})
+
+console.log('open http://localhost:8888 这个端口号超酷耶' )
+server.listen(8888)
+
+//值得思考的内容：
+//setHeader的作用、Content-Type的作用、状态码、乱码的解决
+//服务器的本质、HTTP协议的本质
